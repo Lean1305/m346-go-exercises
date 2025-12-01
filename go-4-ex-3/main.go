d9064a208c24843584d3f551a7fa6c6eef4e36bb
Your Name <you@example.com>
2025-12-01 15:11:29 +0100

    Go 4 Aufgabe 3 fertig

--- go-4-ex-3/main.go
+++ go-4-ex-3/main.go
@@ -1,7 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 // TODO: implement the function computeQuadraticFormula
+func computeQuadraticFormula(a, b, c float64) []float64 {
+	D := b*b - 4*a*c
+
+	if D > 0 {
+		return []float64{
+			(-b + math.Sqrt(D)) / (2 * a),
+			(-b - math.Sqrt(D)) / (2 * a),
+		}
+	}
+
+	if D == 0 {
+		return []float64{
+			-b / (2 * a),
+		}
+	}
+
+	return []float64{} // keine Lösung
+}
 
 func main() {
-	// TODO: call the function computeQuadraticFormula
+	fmt.Println(computeQuadraticFormula(3, 4, 1)) // zwei Lösungen
+	fmt.Println(computeQuadraticFormula(2, 4, 2)) // eine Lösung
+	fmt.Println(computeQuadraticFormula(3, 4, 2)) // keine Lösung
 }
