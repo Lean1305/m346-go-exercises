67c9df3881caa08ee3ede63487ef202f5c2397b7
Your Name <you@example.com>
2025-12-01 14:45:27 +0100

    Go 4 Aufgabe 1 Fertig

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,15 @@
 package main
 
+import "fmt"
+
 // TODO: implement the function computeGrade
+func computeGrade(gotPoints float64, maxPoints float64) float64 {
+	return 1.0 + (gotPoints/maxPoints)*5.0
+}
 
 func main() {
 	// TODO: call the function computeGrade
+	fmt.Println(computeGrade(17.5, 28.0))
+	fmt.Println(computeGrade(12.0, 20.0))
+	fmt.Println(computeGrade(8.0, 16.0))
 }
